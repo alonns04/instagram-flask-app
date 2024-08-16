f04052ffb07d0972083aa7a0b2375d71cf7a78f6
alonns04 <claudiogabrielalonso@gmail.com>
2024-08-16 02:32:52 -0300

    webhooks

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
-import requests
-from flask import Flask, jsonify
+from flask import Flask, request
 import os
+import requests
 
 app = Flask(__name__)
 
-# Usa la variable de entorno para el token de acceso
-INSTAGRAM_ACCESS_TOKEN = os.getenv('INSTAGRAM_ACCESS_TOKEN')
+def subscribe_webhooks():
+    IG_ID = os.getenv('IG_ID')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+    WEBHOOK_URL = 'https://instagram-flask-app.onrender.com/webhook'
+
+    response = requests.post(
+        f'https://graph.instagram.com/v20.0/{IG_ID}/subscribed_apps',
+        params={
+            'subscribed_fields': 'comments,messages',
+            'access_token': ACCESS_TOKEN
+        }
+    )
+
+    if response.status_code == 200:
+        print('Suscripción exitosa:', response.json())
+    else:
+        print('Error en la suscripción:', response.status_code, response.text)
+
+@app.before_first_request
+def setup_webhooks():
+    subscribe_webhooks()
 
 @app.route('/')
 def hello_world():
-    return '¡Hola, Mundo!'
+    return 'Hello, World!'
 
-@app.route('/instagram/media')
-def get_instagram_media():
-    url = f"https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token={INSTAGRAM_ACCESS_TOKEN}"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return jsonify(data)
-    else:
-        return jsonify({"error": "Error al obtener los datos"}), response.status_code
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print(data)  # Imprime los datos recibidos en la consola para depuración
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
