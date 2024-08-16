4db753ed1195028e4831c40ee15041eb2349b2ea
alonns04 <claudiogabrielalonso@gmail.com>
2024-08-16 02:02:51 -0300

    deploy 0.02

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+# Usa la variable de entorno para el token de acceso
+INSTAGRAM_ACCESS_TOKEN = os.getenv('INSTAGRAM_ACCESS_TOKEN')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '¡Hola, Mundo!'
+
+@app.route('/instagram/media')
+def get_instagram_media():
+    url = f"https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token={INSTAGRAM_ACCESS_TOKEN}"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Error al obtener los datos"}), response.status_code
