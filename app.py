793f7dc6adf92b551a955fbcd6f426564a9917ec
alonns04 <claudiogabrielalonso@gmail.com>
2024-08-25 01:11:48 -0300

    app lista

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('/home.html')
+    return render_template('home.html')  # Sin la barra inicial "/"
 
 if __name__ == '__main__':
     app.run(debug=True)
