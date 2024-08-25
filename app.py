bd93b85a8e2eff6cd3e280f5fc6c2b47f5fcfa15
alonns04 <claudiogabrielalonso@gmail.com>
2024-08-25 01:03:40 -0300

    homehome

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, template_rendered
-import os
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    template_rendered('../home/home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
